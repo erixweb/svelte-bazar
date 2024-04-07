3fd15bdfecb84687c45b937672b299c7247e937f
erixweb <erikmorenodev@gmail.com>
2024-04-07 13:56:56 +0200

    Add autocomplete in searchbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	//import { SearchBarProduct } from "../types"
 	import { goto } from "$app/navigation"
 	import { page } from "$app/stores"
 	import IconSearch from "$lib/IconSearch.svelte"
@@ -7,6 +8,7 @@
 	import json from "../products.json"
 
 	let products = json["products"]
+	let searchItems: any = []
 
 	let search = $page.url.searchParams.get("cerca") || ""
 	let category = $page.url.searchParams.get("categoria") || ""
@@ -26,27 +28,26 @@
 				})
 		}
 	}
+	function handleSearch(searchValue: string) {
+		searchItems = []
+		products
+			.filter((product) => product.title.toLowerCase().includes(searchValue.toLowerCase()))
+			.slice(0, 5)
+			.forEach((product) => {
+				searchItems.push({ name: product.title, href: `/api/items/${product.id}` })
+			})
+	}
 </script>
 
 <svelte:head>
 	<title>Bazar amb Svelte</title>
 </svelte:head>
 <main class="w-full max-w-[50em] p-[25px] m-auto font-sans">
+	<h2 class="text-center font-bold text-[40px]">Bazar</h2>
 	<form
 		class="w-full flex justify-center items-center flex-col"
 		on:submit={(e) => handleSubmit(e)}
 	>
-		<h2 class="text-center font-bold text-[25px]">Cerca</h2>
-		<div
-			class="bg-slate-100 w-full inline-flex items-center max-w-[500px] p-[16px] gap-[10px] rounded-[7px]"
-		>
-			<IconSearch />
-			<input
-				type="text"
-				class="bg-transparent w-full justify-center flex outline-none"
-				bind:value={search}
-			/>
-		</div>
 		<div class="inline-flex mt-[20px] mb-[20px] gap-[20px]">
 			<button
 				class="border-slate-400 border-[1px] text-[16px] p-[6px] rounded-full min-w-[100px]"
@@ -72,6 +73,31 @@
 			>
 				portàtils
 			</button>
+		</div>
+		<div class="w-full max-w-[500px] rounded-[7px] min-h-[240px]">
+			<div
+				class="inline-flex items-center bg-slate-100 gap-[10px] p-[16px] w-full rounded-[5px]"
+			>
+				<IconSearch />
+				<input
+					type="text"
+					class="bg-transparent w-full justify-center flex outline-none"
+					placeholder="Cerca"
+					bind:value={search}
+					on:keyup={() => handleSearch(search)}
+				/>
+			</div>
+			<div class="w-full max-w-[500px]">
+				<ol class="bg-slate-100 p-[16px] rounded-b-[5px]">
+					{#each searchItems as item}
+						<li>
+							<a href={item.href}>
+								{item.name}
+							</a>
+						</li>
+					{/each}
+				</ol>
+			</div>
 		</div>
 	</form>
 	<section class="w-full grid grid-cols-3">
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,20 +1,23 @@
 export type Products = {
-    products: Product[];
-    total:    number;
-    skip:     number;
-    limit:    number;
+	products: Product[]
+	total: number
+	skip: number
+	limit: number
 }
-
+export type SearchBarProduct = {
+	name: string
+    href: string
+}
 export type Product = {
-    id:                 number;
-    title:              string;
-    description:        string;
-    price:              number;
-    discountPercentage: number;
-    rating:             number;
-    stock:              number;
-    brand:              string;
-    category:           string;
-    thumbnail:          string;
-    images:             string[];
+	id: number
+	title: string
+	description: string
+	price: number
+	discountPercentage: number
+	rating: number
+	stock: number
+	brand: string
+	category: string
+	thumbnail: string
+	images: string[]
 }
