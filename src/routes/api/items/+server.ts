b855dae6e8973b150b62b4ac4061d8380b1f903b
erixweb <erikmorenodev@gmail.com>
2024-07-10 22:23:35 +0200

    Fix types & Fix API response

--- src/routes/api/items/+server.ts
+++ src/routes/api/items/+server.ts
@@ -1,12 +1,12 @@
 import { filter } from "$lib/filter"
 import json  from "../../../products.json"
 
-export const GET = ({ url }) => {
+export const GET = ({ url }: { url: URL}) => {
     const products = json["products"]
 
     const query = url.searchParams.get("cerca")!
     const category = url.searchParams.get("categoria")
 
-
+    
     return new Response(JSON.stringify(filter(products, { query, category })))
 }
